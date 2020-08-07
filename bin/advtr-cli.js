1b8c93d598844478b27394459b28613a5dd50927
Harry Wright <haroldtomwright@gmail.com>
2020-08-07 12:04:27 +0100

    bin: Fix default_tag
    Defaults tag was ignored due to a change of camelCase and snake_case
    
    Will be filled fixed in #2 with the check for both

--- bin/advtr-cli.js
+++ bin/advtr-cli.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 (function () {
-  process.title = 'advtr';
+  process.title = 'advtr'
 
-  const fs = require('fs');
-  const path = require('path');
-  const assert = require('assert');
-  const childProcess = require('child_process');
-  const util = require('util');
+  const fs = require('fs')
+  const path = require('path')
+  const assert = require('assert')
+  const childProcess = require('child_process')
+  const util = require('util')
 
-  const ora = require('ora');
-  const nopt = require('nopt');
+  const ora = require('ora')
+  const nopt = require('nopt')
 
-  const debug = util.debuglog('advtr');
-  const exec = util.promisify(childProcess.exec);
+  const debug = util.debuglog('advtr')
+  const exec = util.promisify(childProcess.exec)
 
   const opts = nopt({
     file: path,
@@ -22,14 +22,14 @@
     push: Boolean,
     tag: [Array, String],
     usage: Boolean,
-    user: [null, String],
+    user: [null, String]
   }, {
-    'help': ['--usage'],
-    'T': ['--tag'],
-    'U': ['--user']
-  });
+    help: ['--usage'],
+    T: ['--tag'],
+    U: ['--user']
+  })
 
-  const _ = require('lodash');
+  const _ = require('lodash')
   const config = _.defaults(opts, {
     file: process.cwd(),
     loglevel: 'notice',
@@ -37,8 +37,8 @@
     push: false,
     tag: null,
     usage: false,
-    user: null,
-  });
+    user: null
+  })
 
   if (config.usage) {
     console.log([
@@ -61,94 +61,94 @@
     'build',
     'post_build',
     'push',
-    'post_push',
-  ]);
+    'post_push'
+  ])
 
-  function DockerImage(data, build) {
-    this.build = build;
+  function DockerImage (data, build) {
+    this.build = build
 
-    this.name = data.name;
-    this.dockerfile = path.join(config.file, data.dockerfile || '.');
-    this.defaultTag = data.defaultTag || 'latest';
+    this.name = data.name
+    this.dockerfile = path.join(config.file, data.dockerfile || '.')
+    this.defaultTag = data.default_tag || 'latest'
 
-    this.before_build = data.before_build;
-    this.post_build = data.post_build;
-    this.post_push = data.post_push;
+    this.before_build = data.before_build
+    this.post_build = data.post_build
+    this.post_push = data.post_push
 
-    this.parsedBuildArgs = new Map([]);
+    this.parsedBuildArgs = new Map([])
     if (data.build_arguments) {
-      const args = { map: new Map(), types: { } };
+      const args = { map: new Map(), types: { } }
       // Reads the original options from the yaml
       data.build_arguments.forEach((argument) => {
-        const [value, key] = argument.split('=');
+        const [value, key] = argument.split('=')
 
         // This is for nopt to read the args
-        args.types[key] = String;
+        args.types[key] = String
 
         // This is a small cache for the build
         // argument and its env variable counterpart
-        args.map.set(key, value);
-      });
+        args.map.set(key, value)
+      })
 
-      this.buildArgs = args.map;
-      const currentConfigs = nopt(args.types, { }, config.argv.original, 0);
+      this.buildArgs = args.map
+      const currentConfigs = nopt(args.types, { }, config.argv.original, 0)
 
       Object.keys(currentConfigs).forEach((key) => {
         if (args.map.has(key)) {
-          this.parsedBuildArgs.set(args.map.get(key), currentConfigs[key]);
+          this.parsedBuildArgs.set(args.map.get(key), currentConfigs[key])
         }
-      });
+      })
 
       if (!config['pass-args'] && this.parsedBuildArgs.size !== args.map.size) {
-        const missing = [];
+        const missing = []
         args.map.forEach((value, key) => {
-          if (this.buildArgs.has(value)) return;
-          missing.push(`--${key}`);
-        });
+          if (this.buildArgs.has(value)) return
+          missing.push(`--${key}`)
+        })
 
-        const error = new Error('Invalid build arguments');
-        error.code = 1001;
-        error.missingArguments = missing;
-        throw error;
+        const error = new Error('Invalid build arguments')
+        error.code = 1001
+        error.missingArguments = missing
+        throw error
       }
     }
 
-    this.removeArtifacts = data.rm || false;
+    this.removeArtifacts = data.rm || false
   }
 
-  function Registry(data, hub = false) {
-    this.name = data.name;
-    this.url = data.url;
+  function Registry (data, hub = false) {
+    this.name = data.name
+    this.url = data.url
 
-    this.hub = hub;
+    this.hub = hub
   }
 
-  let registry = new Registry({ name: 'docker' }, true);
+  let registry = new Registry({ name: 'docker' }, true)
 
-  function Stage(stage, cmds) {
-    this.stage = stage;
-    this.commands = cmds;
+  function Stage (stage, cmds) {
+    this.stage = stage
+    this.commands = cmds
   }
 
-  function createStage(stage, image) {
-    if (!image[stage]) return null;
-    return new Stage(stage, image[stage]);
+  function createStage (stage, image) {
+    if (!image[stage]) return null
+    return new Stage(stage, image[stage])
   }
 
-  function createBuildStage(image) {
+  function createBuildStage (image) {
     const createTag = (image_, tag) => {
-      const base = `${registry.name === 'docker' ? config.user || require('os').userInfo().username : registry.url}`;
-      return `-t ${base}/${image_.name}:${tag}`;
-    };
+      const base = `${registry.name === 'docker' ? config.user || require('os').userInfo().username : registry.url}`
+      return `-t ${base}/${image_.name}:${tag}`
+    }
 
     const createEnvironmentVariables = (image_) => {
-      const variables = [];
+      const variables = []
       image_.parsedBuildArgs.forEach((value, key) => {
-        variables.push(`--build-arg ${key}=${value}`);
-      });
+        variables.push(`--build-arg ${key}=${value}`)
+      })
 
-      return variables.join();
-    };
+      return variables.join()
+    }
 
     return new Stage('build', [
       [
@@ -157,16 +157,16 @@
         Array.isArray(config.tag) ? (config.tag.map((tag) => createTag(image, tag))).join(' ') : createTag(image, config.tag || image.defaultTag),
         createEnvironmentVariables(image),
         image.removeArtifacts ? '--rm' : null,
-        `${path.relative(process.cwd(), image.dockerfile) || '.'}`,
-      ].filter((el) => el).join(' ').trim(),
-    ]);
+        `${path.relative(process.cwd(), image.dockerfile) || '.'}`
+      ].filter((el) => el).join(' ').trim()
+    ])
   }
 
-  function createPushStage(image) {
+  function createPushStage (image) {
     const createTag = (image_, tag) => {
-      const base = `${registry.name === 'docker' ? config.user || require('os').userInfo().username : registry.url}`;
-      return `'${base}/${image_.name}:${tag}'`;
-    };
+      const base = `${registry.name === 'docker' ? config.user || require('os').userInfo().username : registry.url}`
+      return `'${base}/${image_.name}:${tag}'`
+    }
 
     // https://github.com/docker/cli/issues/267#issuecomment-511052637
     return new Stage('push', [
@@ -184,20 +184,20 @@
 
   // set status to 0 so we can build it before creation
   // will move it to
-  function Builder(image, build, status = 0) {
-    this.build = build;
-    this.status = status;
-    this.tag = config.tag || image.defaultTag;
+  function Builder (image, build, status = 0) {
+    this.build = build
+    this.status = status
+    this.tag = config.tag || image.defaultTag
 
-    this.name = image.name;
+    this.name = image.name
 
     this.stages = [
       createStage('before_build', image),
       createBuildStage(image),
       createStage('post_build', image),
       config.push ? createPushStage(image) : null,
-      createStage('post_push', image),
-    ].filter((el) => el);
+      createStage('post_push', image)
+    ].filter((el) => el)
   }
 
   Builder.BuildStatus = {
@@ -207,85 +207,85 @@
     POST_BUILD: 3,
     PUSH: 4,
     POST_PUSH: 5,
-    COMPLETE: -1,
-  };
+    COMPLETE: -1
+  }
 
-  Builder.BuildStatusMap = { };
+  Builder.BuildStatusMap = { }
   Object.keys(Builder.BuildStatus).forEach((key) => {
-    Builder.BuildStatusMap[key.toLowerCase()] = Builder.BuildStatus[key];
-  });
+    Builder.BuildStatusMap[key.toLowerCase()] = Builder.BuildStatus[key]
+  })
 
   Builder.prototype.run = async function (stage, spinner = { text: '', fail: () => { } }) {
-    this.status = Builder.BuildStatusMap[stage];
+    this.status = Builder.BuildStatusMap[stage]
     // eslint-ignore no-param-reassign
-    spinner.text = `Running ${stage}`;
+    spinner.text = `Running ${stage}`
 
     const commands = this.stages
       .filter((stage_) => stage_.stage === stage)
-      .map((stage_) => stage_.commands).flat();
+      .map((stage_) => stage_.commands).flat()
 
     for (let i = 0; i < commands.length; i++) {
-      const command = commands[i];
-      debug(`\n$ ${command}`);
+      const command = commands[i]
+      debug(`\n$ ${command}`)
 
       try {
-        const { stdout } = await exec(command);
+        const { stdout } = await exec(command)
 
-        debug('');
-        debug(stdout);
+        debug('')
+        debug(stdout)
       } catch (err) {
-        spinner.fail(err.message);
-        process.exit(1);
+        spinner.fail(err.message)
+        process.exit(1)
       }
     }
-  };
-
-  const loadingSpinner = ora('Loading .advtrc').start();
-
-  let data;
-  try {
-    const fileContents = fs.readFileSync(path.join(config.file, '.advtrc'), 'utf8');
-    data = require('js-yaml').safeLoad(fileContents);
-
-    assert.strictEqual(parseFloat(data.version), 1.0);
-
-    /** Here is where we should validate the yaml */
-    loadingSpinner.text = 'Validating .advtrc';
-  } catch (e) {
-    loadingSpinner.fail(`Loading .advtrc failed\n\t$ ${e.message}`);
-    process.exit(1);
   }
 
-  let images = [];
+  const loadingSpinner = ora('Loading .advtrc').start()
+
+  let data
+  try {
+    const fileContents = fs.readFileSync(path.join(config.file, '.advtrc'), 'utf8')
+    data = require('js-yaml').safeLoad(fileContents)
+
+    assert.strictEqual(parseFloat(data.version), 1.0)
+
+    /** Here is where we should validate the yaml */
+    loadingSpinner.text = 'Validating .advtrc'
+  } catch (e) {
+    loadingSpinner.fail(`Loading .advtrc failed\n\t$ ${e.message}`)
+    process.exit(1)
+  }
+
+  let images = []
   Object.keys(data).forEach((key) => {
     switch (key) {
-      case 'version': assert(data[key] === '1.0'); break;
+      case 'version': assert(data[key] === '1.0'); break
       case 'services': {
-        const services = data[key];
+        const services = data[key]
         Object.keys(services).forEach((key_) => {
           try {
-            return images.push(new DockerImage(services[key_], key_));
+            return images.push(new DockerImage(services[key_], key_))
           } catch (err) {
-            loadingSpinner.fail(`Reading .advtrc failed\n  $ ${err.message}`);
-            return process.exit(1);
+            loadingSpinner.fail(`Reading .advtrc failed\n  $ ${err.message}`)
+            return process.exit(1)
           }
-        });
-        break;
+        })
+        break
       }
       case 'registry': {
-        const base = data[key];
+        const base = data[key]
         Object.keys(base).forEach((key_) => {
-          const data_ = _.defaults({ name: key_ }, base[key_]);
-          if (key_ === 'docker') return;
-          registry = new Registry(data_);
-        });
-        break;
+          const data_ = _.defaults({ name: key_ }, base[key_])
+          if (key_ === 'docker') return
+          registry = new Registry(data_)
+        })
+        break
       }
-      default: throw new Error('Invalid Case');
+      default: throw new Error('Invalid Case')
     }
-  });
+  })
 
-  loadingSpinner.succeed('Loaded .advtrc');
+  loadingSpinner.succeed('Loaded .advtrc')
 
   /**
    * 1) Check if the remaining argv containers a name of an image
@@ -296,48 +296,48 @@
   // Check if we have added a target to build, if not use all
   let names = images
     .map((image) => image.build)
-    .filter((name) => config.argv.remain.includes(name));
+    .filter((name) => config.argv.remain.includes(name))
 
   // Or just use them all probably an easier way but meh for now
-  if (names.length === 0) names = images.map((image) => image.build);
+  if (names.length === 0) names = images.map((image) => image.build)
 
   // filter the images and reset them to remove the ones not needed
-  images = images.filter((image) => names.includes(image.build));
+  images = images.filter((image) => names.includes(image.build))
 
-  console.time('advtr:build');
-  console.log('\n==> Building Images');
-  console.log(`  ${names.join('\n  ')}`);
-  debug(images);
+  console.time('advtr:build')
+  console.log('\n==> Building Images')
+  console.log(`  ${names.join('\n  ')}`)
+  debug(images)
 
-  const builds = images.map((image) => new Builder(image, image.build));
+  const builds = images.map((image) => new Builder(image, image.build))
 
-  async function run(builds_) {
+  async function run (builds_) {
     for (let i = 0; i < builds_.length; i++) {
-      const build = builds_[i];
+      const build = builds_[i]
 
-      console.log();
-      console.time(`advtr:build:${build.build}`);
+      console.log()
+      console.time(`advtr:build:${build.build}`)
 
-      const spinner = ora(`Running ${build.build}`).start();
-      spinner.stream = process.stdout;
-      spinner.discardStdin = false;
+      const spinner = ora(`Running ${build.build}`).start()
+      spinner.stream = process.stdout
+      spinner.discardStdin = false
 
       for (const command of kGlobalCommands) {
-        await build.run(command, spinner);
+        await build.run(command, spinner)
       }
 
-      spinner.succeed(`Built ${build.build}\n`);
-      console.timeEnd(`advtr:build:${build.build}`);
+      spinner.succeed(`Built ${build.build}\n`)
+      console.timeEnd(`advtr:build:${build.build}`)
     }
   }
 
   run(builds).then(() => {
-    console.log();
-    console.timeEnd('advtr:build');
+    console.log()
+    console.timeEnd('advtr:build')
   }).catch((error) => {
-    console.timeEnd('advtr:build');
-    console.error(error);
+    console.timeEnd('advtr:build')
+    console.error(error)
 
-    process.exit(1);
-  }).finally(() => process.exit(0));
-}());
+    process.exit(1)
+  }).finally(() => process.exit(0))
+}())
