0da3dadf103c413e8fa593358a7b805e3505606a
harrytwright <haroldtomwright@gmail.com>
2020-08-07 13:54:13 +0100

    [skip ci] lint: Standardise
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- test/_utils/env.js
+++ test/_utils/env.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process')
 
 module.exports.username = os.userInfo().username
 
-module.exports.git = function(cb) {
+module.exports.git = function (cb) {
   exec('basename -s .git `git config --get remote.origin.url`', (er, output) => {
     if (er) return cb(er)
     return cb(null, output.trim())
